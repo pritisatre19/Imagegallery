d81f36b981a927aafaa0d1df283920b15a4c3b1d
pritisatre19 <satrepriti793@gmail.com>
2026-01-06 16:33:52 +0530

    Fix image paths in lightbox

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let images = [
-    "images/img1.jpg",
-    "images/img2.jpg",
-    "images/img3.jpg",
-    "images/img4.jpg",
-    "images/img5.jpg",
-    "images/img6.jpg"
+    "img1.jpg",
+    "img2.jpg",
+    "img3.jpg",
+    "img4.jpg",
+    "img5.jpg",
+    "img6.jpg"
 ];
 
 let currentIndex = 0;
@@ -28,3 +28,4 @@ function prevImage() {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     document.getElementById("lightbox-img").src = images[currentIndex];
 }
+
